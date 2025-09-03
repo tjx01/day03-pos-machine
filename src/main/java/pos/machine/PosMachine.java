b5e1e9840a364c74059a9e20756bd4dee9bc27e3
TULE <TULE@OOCL.COM>
2025-09-03 19:41:53 +0800

    add formatBeginAndEnd func

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,6 +3,11 @@ package pos.machine;
 
 public class PosMachine {
 
+    void formatBeginAndEnd(StringBuilder receiptBuilder) {
+        receiptBuilder.insert(0, "***<store earning no money>Receipt***\n");
+        receiptBuilder.append("**********************");
+    }
+
     Item existProduct(String barcode) {
         return ItemsLoader.loadAllItems().stream()
                 .filter(item -> item.getBarcode().equals(barcode))
