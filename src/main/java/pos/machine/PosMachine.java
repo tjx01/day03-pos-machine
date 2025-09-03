d38bfdb3e89384b5f396129c1532f5f8d1a87d37
TULE <TULE@OOCL.COM>
2025-09-03 19:42:40 +0800

    add formatReceipt func

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,7 +1,28 @@
 package pos.machine;
 
 
+import java.util.Map;
+
 public class PosMachine {
+
+    void formatReceipt(StringBuilder receiptBuilder, Map<String, Integer> barcodeMap) {
+        int totalPrice = 0;
+        for (Map.Entry<String, Integer> entry : barcodeMap.entrySet()) {
+            String barcode = entry.getKey();
+            int count = entry.getValue();
+            Item item = existProduct(barcode);
+            if (item != null) {
+                int itemTotalPrice = item.getPrice() * count;
+                totalPrice += itemTotalPrice;
+                receiptBuilder.append(String.format("Name: %s, Quantity: %d, Unit price: %d (yuan), Subtotal: %d (yuan)\n",
+                        item.getName(), count, item.getPrice(), itemTotalPrice));
+            } else {
+                throw new RuntimeException("Unknown barcode: " + barcode);
+            }
+        }
+        receiptBuilder.append("----------------------\n");
+        receiptBuilder.append(String.format("Total: %d (yuan)\n", totalPrice));
+    }
 
     void formatBeginAndEnd(StringBuilder receiptBuilder) {
         receiptBuilder.insert(0, "***<store earning no money>Receipt***\n");
