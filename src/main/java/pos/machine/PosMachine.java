88612781535699901aed7e44d5b1ba7681db754e
TULE <TULE@OOCL.COM>
2025-09-03 19:39:35 +0800

    add existProduct func

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,4 +3,10 @@ package pos.machine;
 
 public class PosMachine {
 
+    Item existProduct(String barcode) {
+        return ItemsLoader.loadAllItems().stream()
+                .filter(item -> item.getBarcode().equals(barcode))
+                .findFirst()
+                .orElse(null);
+    }
 }
